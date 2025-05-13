bc9364e37d5c183a83db79391ba4fc28301b8d31
Saffron Liu <64503620+SapphireHue@users.noreply.github.com>
2025-05-13 10:21:07 -0500

    Basic app bar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,23 @@
 import Link from 'next/link'
+import AppBar from '@mui/material/AppBar';
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
     return (
         <html lang="en">
-        <head><title>Aurelia</title></head>
-        <body className='bg-gray-100'>
-            <h1>Aurelia</h1>
-            <Link href="/database">Database</Link>
-            <main>{children}</main>
-        </body>
+            <head><title>Aurelia Certamen Reader</title></head>
+            <body className='bg-gray-100'>
+                <AppBar position='sticky'>
+                    {/* <Link href='/'><h1 className='text-2xl font-bold'>Aurelia</h1></Link> <Link href="/database">Database</Link> */}
+                    <div className='flex items-center'> {/* I think ideally I would want them aligned to the bottom? */}
+                        <Link href='/' className='mx-4 p-0 my-2 text-2xl font-bold hover:text-gray-200'>Aurelia</Link>
+                        <Link href="/database" className='mx-4 p-0 my-2 hover:text-gray-200'>Database</Link>
+
+                    </div>
+
+
+                </AppBar>
+                <main className=' px-4'>{children}</main>
+            </body>
         </html>
     );
 }
