cffb3809519d6508a3ec480b538cc4249c108db6
Saffron Liu <64503620+SapphireHue@users.noreply.github.com>
2025-05-25 13:09:04 -0500

    Basic sketch of buzzer page

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,15 +6,12 @@ export default function Layout({ children }) {
         <html lang="en">
             <head><title>Aurelia Certamen Reader</title></head>
             <body className='bg-gray-100'>
-                <AppBar position='sticky'>
+                <AppBar position='sticky' className='mb-4'>
                     {/* <Link href='/'><h1 className='text-2xl font-bold'>Aurelia</h1></Link> <Link href="/database">Database</Link> */}
                     <div className='flex items-center'> {/* I think ideally I would want them aligned to the bottom? */}
                         <Link href='/' className='mx-4 p-0 my-2 text-2xl font-bold hover:text-gray-200'>Aurelia</Link>
                         <Link href="/database" className='mx-4 p-0 my-2 hover:text-gray-200'>Database</Link>
-
                     </div>
-
-
                 </AppBar>
                 <main className=' px-4'>{children}</main>
             </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,23 @@
 "use client"
 
+import { useState } from "react";
+
 import '../globals.css'
+import { Button, TextField } from '@mui/material';
 export default function Page() {
-  return <p>Hello world!</p>;
+  let questions; // list of questions to be read
+  const [questionText, setQuestionText] = useState("");
+  const [oldQuestions, setOldQuestions] = useState([]);
+  return (
+    <>
+      <Button variant='contained'>Start/Pause/Resume/etc</Button>
+      <Button variant='contained'> Next/Skip </Button>
+      <label>Answer: <input /></label>
+      <br />
+      <div>{questionText}</div>
+      <ul className="list-none">
+        {oldQuestions.map(q => <QuestionCard question={q} key={q._id} />)}
+      </ul>
+    </>
+  );
 }
